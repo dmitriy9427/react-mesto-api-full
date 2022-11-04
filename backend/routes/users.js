61b317c0064fbd46e85090597742414c884e46e6
Dmitryi Ryabov <dima.poltergeist.2015@yandex.ru>
2022-11-04 19:56:07 +0300

    создал папку backend

---
+++ backend/routes/users.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+
+const {
+  userAvatarValid,
+  parameterIdValid,
+  userValid,
+} = require('../middlewares/joi');
+
+const {
+  getAllUsers,
+  getCurrentUser,
+  getUserById,
+  updateProfile,
+  updateAvatar,
+} = require('../controllers/users');
+
+router.get('/users', getAllUsers);
+router.get('/users/me', getCurrentUser);
+router.get('/users/:userId', parameterIdValid('userId'), getUserById);
+router.patch('/users/me', userValid, updateProfile);
+router.patch('/users/me/avatar', userAvatarValid, updateAvatar);
+
+module.exports = router;
