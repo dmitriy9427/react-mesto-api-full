7f529ba669ee26cafac0449db0b2ed7785784d83
Dmitryi Ryabov <dima.poltergeist.2015@yandex.ru>
2022-11-13 21:34:15 +0300

    4

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,31 +1,29 @@
 const allowedCors = [
-  "https://localhost:3000",
-  "https://localhost:3001",
-  "https://domainname.ryabov1994.nomoredomains.icu",
-  "http://domainname.ryabov1994.nomoredomains.icu"
-  "https://domainname.ryabovdima.nomoredomains.icu",
-  "http://domainname.ryabovdima.nomoredomains.icu"
+  'https://localhost:3000',
+  'http://localhost:3000',
+  'localhost:3000',
+  'https://localhost:3001',
+  'https://domainname.ryabov1994.nomoredomains.icu',
+  'https://domainname.ryabovdima.nomoredomains.icu',
 ];
 
 const cors = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
   // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
-  const requestHeaders = req.headers["access-control-request-headers"];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
   console.log(req);
-
+  res.header('Access-Control-Allow-Credentials', true);
   if (allowedCors.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Origin', origin);
   }
-  if (method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
-    res.header("Access-Control-Allow-Headers", requestHeaders);
-    res.send();
-    return;
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
   }
-  next();
+  return next();
 };
 
 module.exports = cors;
