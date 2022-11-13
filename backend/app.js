5a43de606746e7b68621f7c67d5fdbbccf98cd2a
Dmitryi Ryabov <dima.poltergeist.2015@yandex.ru>
2022-11-13 22:26:14 +0300

    f

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,17 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const { errors } = require('celebrate');
-const routesUsers = require('./routes/users');
-const routesCards = require('./routes/cards');
-const NotFound = require('./errors/NotFound');
-const { createUser, login } = require('./controllers/users');
-const cors = require('./middlewares/cors');
-const auth = require('./middlewares/auth');
-const { registerValid, loginValid } = require('./middlewares/joi');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
-const errorHandler = require('./middlewares/errorHandler');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const { errors } = require("celebrate");
+const routesUsers = require("./routes/users");
+const routesCards = require("./routes/cards");
+const NotFound = require("./errors/NotFound");
+const { createUser, login } = require("./controllers/users");
+const cors = require("./middlewares/cors");
+const auth = require("./middlewares/auth");
+const { registerValid, loginValid } = require("./middlewares/joi");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
+const errorHandler = require("./middlewares/errorHandler");
 
 const { PORT = 3001 } = process.env;
 
@@ -23,17 +23,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger); // подключаем логгер запросов
 
-app.get('/crash-test', () => {
+app.get("/crash-test", () => {
   setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
+    throw new Error("Сервер сейчас упадёт");
   }, 0);
 });
 
 // логин
-app.post('/signin', loginValid, login);
+app.post("/signin", loginValid, login);
 
 // регистрация
-app.post('/signup', registerValid, createUser);
+app.post("/signup", registerValid, createUser);
 
 app.use(auth);
 
@@ -42,7 +42,7 @@ app.use(routesUsers);
 app.use(routesCards);
 
 app.use(() => {
-  throw new NotFound('Запрашиваемый ресурс не найден');
+  throw new NotFound("Запрашиваемый ресурс не найден");
 });
 
 app.use(errorLogger); // подключаем логгер ошибок
@@ -52,7 +52,7 @@ app.use(errors());
 // централизованный обработчик ошибок
 app.use(errorHandler);
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+mongoose.connect("mongodb://127.0.0.1:27017/mestodb", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
