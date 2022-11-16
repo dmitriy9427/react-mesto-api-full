b773db374318456c993b1ae22aca94be9b460516
Dmitryi Ryabov <dima.poltergeist.2015@yandex.ru>
2022-11-17 00:01:08 +0300

    заполнил README

--- backend/models/card.js
+++ backend/models/card.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose");
-const { isURL } = require("validator");
+const mongoose = require('mongoose');
+const { isURL } = require('validator');
 
 const cardSchema = new mongoose.Schema({
   name: {
@@ -18,13 +18,13 @@ const cardSchema = new mongoose.Schema({
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "user",
+    ref: 'user',
     required: true,
   },
   likes: [
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "user",
+      ref: 'user',
       default: [],
     },
   ],
@@ -34,4 +34,4 @@ const cardSchema = new mongoose.Schema({
   },
 });
 
-module.exports = mongoose.model("card", cardSchema);
+module.exports = mongoose.model('card', cardSchema);
